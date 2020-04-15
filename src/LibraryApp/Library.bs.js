db46ea962ee2811c81eb3175ec62f75cbc630679
Hasan Ali <hasanhaja@gmail.com>
2020-04-15 19:05:28 +0100

    Product of the week feature implemented, but properties like alt are missing.

--- src/LibraryApp/Library.bs.js
+++ src/LibraryApp/Library.bs.js
@@ -19,7 +19,8 @@ function makeBookRecordFromBookData(bookData, freeBookmark) {
 function makeArrayOfBookElementFromBooks(books, freeBookmark) {
   return Belt_List.toArray(Belt_List.mapWithIndex(books, (function (i, currentBook) {
                     return React.createElement(Book$ReasonReactExamples.make, {
-                                book: makeBookRecordFromBookData(currentBook, freeBookmark)
+                                book: makeBookRecordFromBookData(currentBook, freeBookmark),
+                                key: String(i)
                               });
                   })));
 }
@@ -37,16 +38,43 @@ function Library(Props) {
           return false;
         }));
   var setHiring = match$2[1];
-  React.useState((function () {
-          return false;
+  var match$3 = React.useState((function () {
+          return true;
         }));
+  var setLoading = match$3[1];
+  var match$4 = React.useState((function () {
+          return /* [] */0;
+        }));
+  var setData = match$4[1];
+  React.useEffect((function () {
+          console.log("Using effect");
+          fetch("https://hplussport.com/api/products/order/price/sort/asc/qty/1").then((function (data) {
+                    return data.json();
+                  })).then((function (data) {
+                  Curry._1(setData, (function (param) {
+                          return data;
+                        }));
+                  Curry._1(setLoading, (function (param) {
+                          return false;
+                        }));
+                  return Promise.resolve(/* () */0);
+                }));
+          return ;
+        }), ([]));
   return React.createElement("div", undefined, match$2[0] ? React.createElement(Hiring$ReasonReactExamples.make, { }) : React.createElement(NotHiring$ReasonReactExamples.make, { }), React.createElement("button", {
                   onClick: (function (param) {
                       return Curry._1(setHiring, (function (prev) {
                                     return !prev;
                                   }));
                     })
-                }, "Toggle hiring"), React.createElement("h1", undefined, "The Library is " + (
+                }, "Toggle hiring"), React.createElement("div", undefined, match$3[0] ? React.createElement("h3", undefined, "Loading...") : React.createElement("h3", undefined, Belt_List.toArray(Belt_List.map(match$4[0], (function (product) {
+                                  return React.createElement("div", {
+                                              key: product.id
+                                            }, React.createElement("h3", undefined, "Library product of the week"), React.createElement("h4", undefined, product.name), React.createElement("img", {
+                                                  height: String(100),
+                                                  src: product.image
+                                                }), React.createElement("h4", undefined, "$" + String(product.price)));
+                                }))))), React.createElement("h1", undefined, "The Library is " + (
                   match[0] ? "open." : "closed."
                 )), React.createElement("button", {
                   onClick: (function (param) {
--- src/LibraryApp/Library.re
+++ src/LibraryApp/Library.re
@@ -1,3 +1,5 @@
+[@bs.val] external fetch: string => Js.Promise.t('a) = "fetch";
+
 type libraryData = list(Data.bookData);
 
 let makeBookRecordFromBookData = (~bookData: Data.bookData, ~freeBookmark) => {
@@ -13,7 +15,10 @@ let makeBookRecordFromBookData = (~bookData: Data.bookData, ~freeBookmark) => {
 let makeArrayOfBookElementFromBooks = (~books: libraryData, ~freeBookmark) => {
   books
   ->Belt.List.mapWithIndex((i, currentBook) => {
-      <Book book={makeBookRecordFromBookData(currentBook, freeBookmark)} />
+      <Book
+        key={string_of_int(i)}
+        book={makeBookRecordFromBookData(currentBook, freeBookmark)}
+      />
     })
   ->Belt.List.toArray;
 };
@@ -23,13 +28,50 @@ let make = (~books: libraryData) => {
   let (libOpen, setLibOpen) = React.useState(() => true);
   let (freeBookmark, setFreeBookmark) = React.useState(() => true);
   let (hiring, setHiring) = React.useState(() => false);
-  let (loading, setLoading) = React.useState(() => false);
+  let (loading, setLoading) = React.useState(() => true);
+  let (data, setData) = React.useState(() => []);
+
+  React.useEffect0(() => {
+    Js.log("Using effect");
+
+    Js.Promise.(
+      fetch("https://hplussport.com/api/products/order/price/sort/asc/qty/1")
+      |> then_(data => data##json())
+      |> then_(data => {
+           setData(_ => data);
+           setLoading(_ => false);
+           Js.Promise.resolve();
+         })
+      |> ignore
+    );
+
+    None;
+  });
 
   <div>
     {hiring ? <Hiring /> : <NotHiring />}
     <button onClick={_ => setHiring(prev => !prev)}>
       {React.string("Toggle hiring")}
     </button>
+    <div>
+      {loading
+         ? <h3> {React.string("Loading...")} </h3>
+         : <h3>
+             {data
+              ->Belt.List.map(product =>
+                  <div key={product##id}>
+                    <h3> {React.string("Library product of the week")} </h3>
+                    <h4> {product##name} </h4>
+                    <img src={product##image} height={string_of_int(100)} />
+                    <h4>
+                      {React.string("$" ++ string_of_int(product##price))}
+                    </h4>
+                  </div>
+                )
+              ->Belt.List.toArray
+              ->React.array}
+           </h3>}
+    </div>
     <h1>
       {React.string("The Library is " ++ (libOpen ? "open." : "closed."))}
     </h1>
